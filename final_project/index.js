92804dd071e56f77a64f7748d1e6f71967c5acde
solomonadola <solomonadola84@gmail.com>
2024-01-10 10:39:19 -0500

    Nodemodule final project

--- final_project/index.js
+++ final_project/index.js
@@ -8,15 +8,36 @@ const app = express();
 
 app.use(express.json());
 
-app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
+app.use("/customer", session({ secret: "fingerprint_customer", resave: true, saveUninitialized: true }))
 
-app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+app.use("/customer/auth/*", function auth(req, res, next) {
+    // write auth logic with express-session from the request object
+    if (req.session.authorization) {
+        const username = req.session.authorization.username;
+        console.log(`AUTHORIZING: User: ${username}`);
+        //   console.log(JSON.stringify(req.session.authorization));
+
+        let token = req.session.authorization.accessToken;
+        // console.log(token);
+        jwt.verify(token, SECRET_KEY, (err, decoded) => {
+            if (!err) {
+                // req.user = decoded;
+                console.log("AUTHORIZING SUCCESSFUL");
+                next();
+            } else {
+                console.log("Error: " + err);
+                res.status(401).send("Unauthorized: You are not Authenticated");
+            }
+        });
+    } else {
+        console.log("AUTHORIZING UNSUCCESSFUL: user not logged in");
+        res.status(401).send("Unauthorized: You are not logged in!");
+    }
 });
- 
-const PORT =5000;
+
+const PORT = 5001;
 
 app.use("/customer", customer_routes);
 app.use("/", genl_routes);
 
-app.listen(PORT,()=>console.log("Server is running"));
+app.listen(PORT, () => console.log("Server is running"));
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -5,39 +5,46 @@ let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
 
-public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.post("/register", (req, res) => {
+    const {username, password} = req.body
+    const user = users.filter(user=>user.username==username)
+
+    if(user.length > 0){
+        return res.send("User already exist")
+    }
+  
+        users.push({username, password})
+    
+    return res.status(300).json({ message: "registred successfully" });
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/', function (req, res) {
+    return res.send(JSON.stringify(books));
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
- });
-  
+public_users.get('/isbn/:isbn', function (req, res) {
+    let book = books[req.params.isbn]
+    return res.send(JSON.stringify(book));
+});
+
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/author/:author', function (req, res) {
+    let booksByAuthor = Object.values(books).filter(book => book.author == req.params.author)
+    return res.status(300).json({ message: booksByAuthor });
 });
 
 // Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/title/:title', function (req, res) {
+    let booksByTitle = Object.values(books).filter(book => book.title == req.params.title)
+    return res.status(300).json({ message: booksByTitle });
 });
 
 //  Get book review
-public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+public_users.get('/review/:isbn', function (req, res) {
+    const bookReview = books[req.params.isbn]['reviews']
+    return res.status(300).json({ message: bookReview });
 });
 
 module.exports.general = public_users;
